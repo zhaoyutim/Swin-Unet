1edc6ee62c899900d264ca620579b50b80888c71
赵宇 <zhaoyu@vpn10-244.remote-access.kth.se>
2022-03-02 17:59:19 +0100

    update config

--- datasets/dataset_palsar.py
+++ datasets/dataset_palsar.py
@@ -51,8 +51,8 @@ class PalsarDataset(Dataset):
         self.transform = transform  # using transform in torch!
         self.split = split
         self.data_dir = base_dir
-        self.train_dataset = np.load('/geoinfo_vol1/zhao2/proj2_dataset/proj2_train.npy')
-        # self.train_dataset = np.load('/Users/zhaoyu/PycharmProjects/ee_fire_monitoring/dataset/proj2_test.npy')
+        # self.train_dataset = np.load('/geoinfo_vol1/zhao2/proj2_dataset/proj2_train.npy')
+        self.train_dataset = np.load('/Users/zhaoyu/PycharmProjects/ee_fire_monitoring/dataset/proj2_test.npy')
         self.y_dataset = self.train_dataset[:,:,:,3]>0
         self.image, self.image_val, self.label, self.label_val = train_test_split(self.train_dataset[:,:,:,:3], self.y_dataset, test_size=0.2, random_state=0)
 

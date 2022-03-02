02266519cb17f9e1c66f5b526533e1b0999fa330
赵宇 <zhaoyu@vpn10-81.remote-access.kth.se>
2022-03-02 21:53:25 +0100

    update config

--- trainer.py
+++ trainer.py
@@ -76,7 +76,7 @@ def trainer_palsar(args, model, snapshot_path):
 
             logging.info('iteration %d : loss : %f, loss_ce: %f' % (iter_num, loss.item(), loss_ce.item()))
 
-            if iter_num % 20 == 0:
+            if iter_num % 100 == 0:
                 image = image_batch[1, 0:1, :, :]
                 image = (image - image.min()) / (image.max() - image.min())
                 writer.add_image('train/Image', image, iter_num)
